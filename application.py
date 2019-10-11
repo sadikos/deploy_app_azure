daec44429483440e24d1b4098a7c4fbcd4d546b9
apps funy <appsfuny@gmail.com>
2019-10-11 13:52:03 +0100

    get user ip @

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>Hello from  Azure cloud </h1>"
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return "hello from cloud  , you ip address is : "+str(jsonify({'ip': request.remote_addr}), 200)
+
+
 @app.route("/isem")
 def isem():
     return "<h1>Hello ISEM</h1>"
